7efb43195703abca6bd3fbc86a43b05500b138d3
shinytsing <1009383129@qq.com>
2025-08-03 07:42:02 +0800

    2025:08:03 07:42:02

--- git_push.sh
+++ git_push.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # 一键提交到GitHub脚本
-# 使用方法: ./git_push.sh "提交信息"
+# 使用方法: ./git_push.sh [可选提交信息]
 
-# 检查是否提供了提交信息
+# 获取当前时间作为默认提交信息
+current_time=$(date '+%Y:%m:%d %H:%M:%S')
+
+# 如果提供了提交信息参数，则使用提供的信息，否则使用当前时间
 if [ $# -eq 0 ]; then
-    echo "请提供提交信息"
-    echo "使用方法: ./git_push.sh \"提交信息\""
-    exit 1
+    commit_message="$current_time"
+else
+    commit_message="$1"
 fi
-
-# 获取提交信息
-commit_message="$1"
 
 echo "开始一键提交到GitHub..."
 
